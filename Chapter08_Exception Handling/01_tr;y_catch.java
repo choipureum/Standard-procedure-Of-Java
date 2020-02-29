decced031ab930fdef56876065cc0c4d0bade4d5
Choipureum <55127127+choipureum@users.noreply.github.com>
2020-02-29 22:00:24 +0900

    #예외처리 (try_catch)문

---
+++ Chapter08_Exception Handling/01_tr;y_catch.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {	
+	public static void main(String[] args) throws IOException, Exception {
+		 try {
+			 //예외가 발생할 수 있는 문장들을 넣느다
+		 }catch(Exception e1){
+			 //Exception e1이 발생했을경우 이를 처리하기위한 문장
+		 }catch(Exception e2){
+			 //Exception e2이 발생했을경우 이를 처리하기위한 문장
+		 }catch(Exception en){
+			 //Exception en이 발생했을경우 이를 처리하기위한 문장
+		 }
+
+	}
+	
+}
